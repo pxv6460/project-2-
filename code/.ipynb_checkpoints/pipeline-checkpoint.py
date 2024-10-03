dfc02c736994dabba6d1298f13ea1b5838ae3d82
Peter Vitello <petervitello2@gmail.com>
2024-10-03 19:07:10 -0400

    done with the main code

--- code/.ipynb_checkpoints/pipeline-checkpoint.py
+++ code/.ipynb_checkpoints/pipeline-checkpoint.py
@@ -22,7 +22,9 @@ def preprocess_data(df, target, columns_to_drop):
     """
     df = df
     dff = df.replace([np.inf, -np.inf], np.nan)
-    dff = dff.fillna(dff.mean())
+    # makes sure all the columns that run the mean are numeric 
+    numeric_cols = dff.select_dtypes(include=[np.number]).columns
+    dff[numeric_cols] = dff[numeric_cols].fillna(dff[numeric_cols].mean())
     
     X = dff.drop(columns=[target] + columns_to_drop)
     y = dff[target]
@@ -58,8 +60,6 @@ def run_model(new_df, train_test_res):
     print(classification_report(train_test_res[3], svc_pred, labels = [1, 0]))
     print("------------------------------------------------------------------------")
     
-
-    
     #svc resampled underfitting 
     clfr_svc_und = SVC(kernel="rbf", random_state=42)
     clfr_svc_und.fit(train_test_res[4], train_test_res[5])
@@ -67,7 +67,6 @@ def run_model(new_df, train_test_res):
     print("SVC undersampled")
     print(classification_report(train_test_res[3], svc_und_pred, labels = [1, 0]))
     print("------------------------------------------------------------------------")
-    
 
     # random forest
     clfr_rf = RandomForestClassifier(n_estimators=500, random_state = 42)
@@ -77,7 +76,6 @@ def run_model(new_df, train_test_res):
     print(classification_report(train_test_res[3], rf_pred, labels = [1, 0]))
     print("------------------------------------------------------------------------")
     
-    
     # underfitting resampled random forest 
     clfr_rf_und = RandomForestClassifier(n_estimators=500, random_state = 42)
     clfr_rf_und.fit(train_test_res[4], train_test_res[5])
@@ -86,9 +84,8 @@ def run_model(new_df, train_test_res):
     print(classification_report(train_test_res[3], rf_und_pred, labels = [1, 0]))
     print("------------------------------------------------------------------------")
     
-    
     #log reggession 
-    logreg = LogisticRegression()
+    logreg = LogisticRegression(random_state = 42)
     logreg.fit(train_test_res[0], train_test_res[2])
     log_pred = logreg.predict(train_test_res[1])
     print("log regg")
@@ -96,13 +93,42 @@ def run_model(new_df, train_test_res):
     print("------------------------------------------------------------------------")
 
     #log reggession 
-    logreg_und = LogisticRegression()
+    logreg_und = LogisticRegression(random_state = 42)
     logreg_und.fit(train_test_res[4], train_test_res[5])
     log_und_pred = logreg_und.predict(train_test_res[1])
     print("log undersampled")
     print(classification_report(train_test_res[3], log_und_pred, labels = [1, 0]))
     print("------------------------------------------------------------------------")
-    
+
+    #knn 
+    knn = KNeighborsClassifier(n_neighbors = 5)
+    knn.fit(train_test_res[0], train_test_res[2])
+    knn_pred = knn.predict(train_test_res[1])
+    print("KNN")
+    print(classification_report(train_test_res[3], knn_pred, labels = [1, 0]))
+    print("------------------------------------------------------------------------")
+
+    #knn undersampled 
+    knn_und = KNeighborsClassifier(n_neighbors = 5)
+    knn_und.fit(train_test_res[4], train_test_res[5])
+    knn_und_pred = knn_und.predict(train_test_res[1])
+    print("KNN undersampled")
+    print(classification_report(train_test_res[3], knn_und_pred, labels = [1, 0]))
+    print("------------------------------------------------------------------------")
+
+    dectree = DecisionTreeClassifier(random_state=42)
+    dectree.fit(train_test_res[0], train_test_res[2])
+    dectree_pred = dectree.predict(train_test_res[1])
+    print("DecisionTree")
+    print(classification_report(train_test_res[3], dectree_pred, labels = [1, 0]))
+    print("------------------------------------------------------------------------")
+
+    dectree_und = DecisionTreeClassifier(random_state=42)
+    dectree_und.fit(train_test_res[4], train_test_res[5])
+    dectree_und_pred = dectree_und.predict(train_test_res[1])
+    print("DecisionTree undersampled")
+    print(classification_report(train_test_res[3], dectree_und_pred, labels = [1, 0]))
+    print("------------------------------------------------------------------------")
     
     # Print the results
     #print("SVC")
